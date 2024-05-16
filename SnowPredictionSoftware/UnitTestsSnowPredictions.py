935532fb086619e4346c6f5db7976a57730a3de4
FrankVanris2 <frankcvanris@gmail.com>
2024-05-16 14:01:40 -0700

    updated unit testing
    updated with new unit tests

--- SnowPredictionSoftware/UnitTestsSnowPredictions.py
+++ SnowPredictionSoftware/UnitTestsSnowPredictions.py
@@ -1,5 +1,5 @@
 '''
-By: Frank Vanris, Stefana Ciustea
+By: Frank Vanris, Stefana Ciustea, Conrad Nark
 Date: 5/14/2024
 Desc: Creating the Unit Tests
 '''
@@ -9,14 +9,23 @@ import unittest
 #This class will inform the test cases that we would like to test to see if our snow prediction site works or not.
 class TestSnowPrediction(unittest.TestCase):
 
-    #testing UI
-    def test_UI(self):
-        self.assertEqual(UI_ON(), 'true')
 
+    #testing Data / KNN Algorithm
+    def test_Retrieve_Data(self):
+        self.assertTrue(getData())
     #testing prediction accuracy is above 80%
     def test_KNN_Accuracy(self):
         self.assertTrue(accuracyKNN() > 80)
 
+    #if accuracy is above most learning models
+    def test_KNN_Accuracy_AboveAverage(self):
+        self.assertTrue(accuracyKNN() > 90)
+
+    #UI / Server
     #testing connection to server
     def test_connection_HTTP(self):
         self.assertEqual(HTTPCon(), 'true')
+
+    # testing UI
+    def test_UI(self):
+        self.assertEqual(UI_ON(), 'true')
